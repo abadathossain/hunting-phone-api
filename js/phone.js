42ac3924d499a2e36690af6c8ae6e8aa061215ca
Abadat Hossain <abadat26@gmail.com>
2024-10-02 06:44:30 +0600

    show all button done after slice

--- js/phone.js
+++ js/phone.js
@@ -1,20 +1,31 @@
-const loadPhones=async(searchText)=>{
-    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    const data=await res.json()
-    const phones=data.data
+const loadPhones = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json()
+    const phones = data.data
     displayPhones(phones)
 }
 
-const displayPhones=(phones)=>{
+const displayPhones = (phones) => {
     // console.log(phones)
-    const phoneContainer=document.getElementById('phone-container')
-    phoneContainer.textContent=''
-    phones.forEach(phone=>{
+    const phoneContainer = document.getElementById('phone-container')
+
+    phoneContainer.textContent = ''
+
+    const showAllButton = document.getElementById('show-all-button')
+    if (phones.length > 12) {
+        showAllButton.classList.remove('hidden')
+    } else {
+        showAllButton.classList.add('hidden')
+    }
+    // set phones number slice for short shown
+    phones = phones.slice(0, 6)
+
+    phones.forEach(phone => {
         // console.log(phone)
 
-const phoneDiv=document.createElement('div')
-phoneDiv.classList="card bg-base-100 w-96 shadow-xl"
-phoneDiv.innerHTML=`
+        const phoneDiv = document.createElement('div')
+        phoneDiv.classList = "card bg-base-100 w-96 shadow-xl"
+        phoneDiv.innerHTML = `
  <div class="card bg-base-100 w-96 shadow-xl">
                     <figure class="px-10 pt-10">
                          <img src="${phone.image}" alt="Phones" class="rounded-xl" />
@@ -29,13 +40,13 @@ phoneDiv.innerHTML=`
                 </div>
 
 `
-phoneContainer.appendChild(phoneDiv)
+        phoneContainer.appendChild(phoneDiv)
 
     })
 }
-const handleSearch=()=>{
-    const searchField=document.getElementById('search-field');
-    const searchText=searchField.value;
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
     // console.log(searchText)
     loadPhones(searchText)
 }
